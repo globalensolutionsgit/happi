299cebcfd4ba8fcc4b7ecec37e4b2ba7ec158fd0
spmuthu21 <muthu@globalensolutions.com>
2015-11-20 17:02:36 +0530

    Changes home page images..

--- header.php
+++ header.php
@@ -18,11 +18,23 @@
 <meta name="viewport" content="width=device-width,initial-scale=1">
 
 <!-- favicon.ico and apple-touch-icon.png -->
-<link rel="shortcut icon" href="favicon.ico">
-<link rel="apple-touch-icon" href="images/apple-touch-icon-57x57-iphone.png">
-<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72-ipad.png">
-<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114-iphone4.png">
-
+<link rel="apple-touch-icon" sizes="57x57" href="fevicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="fevicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="fevicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="fevicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="fevicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="fevicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="fevicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="fevicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="fevicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="fevicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="fevicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="fevicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="fevicon/favicon-16x16.png">
+<link rel="manifest" href="fevicon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="fevicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="screen.css">
 <!-- custom CSS -->
--- index.php
+++ index.php
@@ -5,42 +5,42 @@
 
         	<div class="slides_container">
             	<div class="slide">
-					<img src="images/home/B1.jpg" alt="">
+					<img src="images/home/001.jpg" alt="">
                     <div class="slide_text bottom left">
-						<div class="slide_title"><strong>TAHITI</strong></div>
-	                    <p class="subtitle">Largest island in French Polynesia...</p>
-                    </div>
-			  	</div>
-                <div class="slide">
-					<img src="images/home/B2.jpg" alt="">
-					<div class="slide_text bottom right">
-	                    <div class="slide_title"><strong>INDONESIA</strong></div>
+						<div class="slide_title"><strong>INDONESIA</strong></div>
 	                    <p class="subtitle">Thousands of volcanic islands...</p>
                     </div>
 			  	</div>
                 <div class="slide">
-					<img src="images/home/B3.jpg" alt="">
-                    <div class="slide_text bottom left">
+					<img src="images/home/002.jpg" alt="">
+					<div class="slide_text bottom right">
 	                    <div class="slide_title"><strong>DUBAI</strong></div>
 	                    <p class="subtitle">Luxury shopping, ultramodern architecture and a lively nightlife scene...</p>
                     </div>
 			  	</div>
                 <div class="slide">
-					<img src="images/home/B4.jpg" alt="">
+					<img src="images/home/003.jpg" alt="">
+                    <div class="slide_text bottom left">
+	                    <div class="slide_title"><strong>THAILAND</strong></div>
+	                    <p class="subtitle">Tropical beaches, opulent royal palaces, ancient ruins...</p>
+                    </div>
+			  	</div>
+                <div class="slide">
+					<img src="images/home/004.jpg" alt="">
                     <div class="slide_text left middle">
-	                    <div class="slide_title"><strong>MALAYSIA</strong></div>
-	                    <p class="subtitle">Its beaches, rainforests and mix of Malay...</p>
+	                    <div class="slide_title"><strong>SINGAPORE</strong></div>
+	                    <p class="subtitle">Tropical climate and multicultural population.</p>
                     </div>
 			  	</div>
                 <div class="slide">
-					<img src="images/home/B5.jpg" alt="">
+					<img src="images/home/005.jpg" alt="">
                     <div class="slide_text center top">
-	                    <div class="slide_title"><strong>PATTYA</strong></div>
-	                    <p class="subtitle">Wild nightlife scene that attracts...</p>
+	                    <div class="slide_title"><strong>SRILANKA</strong></div>
+	                    <p class="subtitle">Is a rugged land of rainforest...</p>
                     </div>
 			  	</div>
                 <div class="slide">
-					<img src="images/home/B6.jpg" alt="">
+					<img src="images/home/006.jpg" alt="">
                     <div class="slide_text right middle">
 						<div class="slide_title"><strong>MALAYSIA</strong></div>
 	                    <p class="subtitle">Its beaches, rainforests and mix of Malay...</p>
@@ -51,12 +51,12 @@
             <div class="pagination_wrap">
             	<div class="pagination_inner">
 				<ul class="pagination">
-					<li><a href="#0">TAHITI</a></li>
-	                <li><a href="#1">INDONESIA</a></li>
-	                <li><a href="#2">DUBAI</a></li>
-	                <li><a href="#3">MALAYSIA</a></li>
-	                <li><a href="#4">PATTYA</a></li>
-	                <li><a href="#5">MALAYSIAN</a></li>
+					<li><a href="#0">INDONESIA</a></li>
+	                <li><a href="#1">DUBAI</a></li>
+	                <li><a href="#2">THAILAND</a></li>
+	                <li><a href="#3">SINGAPORE</a></li>
+                    <li><a href="#4">SRILANKA</a></li>
+	                <li><a href="#5">MALAYSIA</a></li>
 	          	</ul>
                 </div>
             </div>
